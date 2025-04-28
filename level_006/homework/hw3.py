8576b2e614dbd0c56b7768ee7457637c1cc6f6de
nikaasanashvili <nikan6215@gmail.com>
2025-04-28 16:36:59 +0400

    goa

--- level_006/homework/hw3.py
+++ level_006/homework/hw3.py
@@ -5,8 +5,8 @@ num1=int(input("number 1: "))
 num2=int(input("number 2:"))
 
 print(num1 - num2) #gamokleba
-print(num1 / num2 ) #nashtiani gayofa
-print(num1 // num2) #unashto gayofa
+print(num1 / num2 ) #gayofa
+print(num1 // num2) #GAMOAQVS PASUXI TU RAMDENJER MOTAVSDEBA 1 RICXVI MEORESHI
 print(num1 % num2) #gamoaqvs nashti
 print(num1 * num2) #gamravleba
 print(num1 + num2) #mimateba
