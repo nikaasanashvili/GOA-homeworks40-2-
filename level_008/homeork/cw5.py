fdf6284a7bcb249bdca35b2f996e86e4415b18b3
nikaasanashvili <nikan6215@gmail.com>
2025-05-07 14:35:40 +0400

    goa

--- level_008/homeork/cw5.py
+++ level_008/homeork/cw5.py
@@ -1,3 +1,3 @@
 # 6)შექმენით ცვლადი სადაც შეინახავთ სტრინგს "konstantinopoli",შენი დავალებააა გამოიტანო ამ სტრინგიდან ბოლო 8 ასო,გამოიყენეთ slicin
 city="konstan tinopoli"
-print(city[7:16])
+print(city[7:])
---
+++ level_009/classwork/cw4.py
@@ -0,0 +1,3 @@
+# 5)შექმენი სია --> ["giorgi","lasha","beqa","aleqsandre",5] შენი დავალებაა გამოიყენო შესაბამისი ფუნქცია და გაიგო რომელ ინდექსზე დგას "aleqsandre"
+list=["giorgi","lasha","beqa","aleqsandre",5]
+print(list.index("aleqsandre"))
