1845d41c59bceaf1658514599185e0612e026b13
nikaasanashvili <nikan6215@gmail.com>
2025-05-10 14:08:40 +0400

    goa

---
+++ level_010/classwork/cw1.py
@@ -0,0 +1,5 @@
+# 2)შექმენით სია სადაც გექნებათ მოთავსებული 7 სახელი,თქვენი დავალებაა მესამე ინდექსზე დაამატო "mtvare" და მეხუთე ინდექსზე დაამატო რიცხვი 65
+name=['nika', 'saba', 'dachi', 'daviti', 'lasha', 'toko', 'gio']
+name.insert(3, 'mtvare')
+name.insert(5, 65)
+print(name)
---
+++ level_010/classwork/cw4.py
@@ -0,0 +1,6 @@
+# 4)შექმენით სია --> ["giorgi" , "lasha", "beqa" , "irakli" , 45] თქვენი დავალებაა ამ სიიდან ამოშალოთ "lasha" და ასევე ამოშალოთ 
+# რიცხვი 45,გამოიყენეთ remove() ფუნქცია
+list=["giorgi" , "lasha", "beqa" , "irakli" , 45] 
+list.remove("lasha")
+list.remove(45)
+print(list)
