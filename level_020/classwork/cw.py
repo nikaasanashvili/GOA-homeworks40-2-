57cf5d48600830558d664ab2aa6fbe11f2341239
nikaasanashvili <nikan6215@gmail.com>
2025-06-11 22:52:14 +0400

    goa

--- level_020/classwork/cw.py
+++ level_020/classwork/cw.py
@@ -1,1 +1,10 @@
-# ar gvqonda
+# 1)შექმენით ფუნქცია რომელსაც გადაეცემა სახელები,თქვენი დავალებაა ტერმინალში გამოიტანოთ კენტ ინდექსზე მდგომი სახელები და შეინახოთ ახალ სიაში
+
+
+def kenti_str(name):
+    kenti_str=""
+    for i in range(0,len(name)+1):
+        if i % 2 == 0:
+            kenti_str += name[i]
+    return kenti_str
+print(kenti_str())
