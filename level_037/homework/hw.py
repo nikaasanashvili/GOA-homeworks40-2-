cdcf9d2bbd4527ad125b60d39fcc84171057e540
nikaasanashvili <nikan6215@gmail.com>
2025-08-13 22:50:22 +0400

    goa

---
+++ level_037/homework/hw.py
@@ -0,0 +1,37 @@
+# codewars
+
+
+# def to_alternating_case(string):
+#     final_txt=''
+#     for i in string:
+#         if i == '1234567890':
+#             final_txt += i
+#         elif i != '1234567890' and i != i.capitalize():
+#             final_txt += i.capitalize()
+#         elif i != '1234567890' and i == i.capitalize():
+#             final_txt += i.lower()
+#     return final_txt
+
+
+# def is_palindrome(s):
+#     palindrom=""
+#     for i in range(1, len(s) + 1 ):
+#         palindrom += s[-i]
+#     return palindrom.lower() == s.lower()
+
+
+# def bonus_time(salary, bonus):
+#     if bonus == True:
+#         return '$' + str(salary * 10)
+#     elif bonus == False:
+#         return '$' + str(salary)
+  
+
+# def merge_arrays(arr1, arr2):
+#     return sorted(set(arr1 + arr2))
+
+
+# def capitalize_word (word : str) -> str:
+#     return word.capitalize()
+
+
