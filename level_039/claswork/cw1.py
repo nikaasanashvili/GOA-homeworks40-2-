ea37f6feb09635f381021d2e71ff3e516c1095f3
nikaasanashvili <nikan6215@gmail.com>
2025-08-20 11:38:45 +0400

    goa

---
+++ level_039/claswork/cw1.py
@@ -0,0 +1,8 @@
+# 2)  კომენტარის სახითბჩამკწერეთ სიის ფუნქციები და გვერდით მიუწერეთ ასევე თუ რომელი რას აკეთებს
+
+list.append() # siis boloshi amatebs frcxilebshi gadacemul mnishvnelobas
+list.remove() # siidan amoshlis frcxilebshi gadacemul mnishvnelobas
+list.index(1) # gamoitans indeqss
+list.pop(1) # amoshlis listshi pirvel indeqszee mdgom elements
+list.insert(1, 'saba') # chaamatebs pirvel indeqsze saba-s
+list.extend() # aertianebs 2 sias
