fa0a39241d4d8fc55ad07138054b8e552a7aed7a
nikaasanashvili <nikan6215@gmail.com>
2025-08-23 18:44:25 +0400

    goa

---
+++ level_040/homework/hw.py
@@ -0,0 +1,63 @@
+# coewars
+
+# def lovefunc( flower1, flower2 ):
+#     if flower1 % 2 == 0:
+#         flower1_is_even = True
+#     else:
+#         flower1_is_even = False
+
+#     if flower2 % 2 == 0:
+#         flower2_is_even = True
+#     else:
+#         flower2_is_even = False
+
+#     if flower1_is_even != flower2_is_even:
+#         return True
+#     else:
+#         return False
+
+
+# def disemvowel(string_):
+#     final_string=''
+#     for i in string_:
+#         if i.lower() not in 'aeiou':
+#             final_string += i
+#     return final_string
+
+
+# def high_and_low(numbers):
+#     int_nambers = numbers.split()
+#     final_list=[]
+#     for i in int_nambers:
+#         final_list.append(int(i))
+        
+
+
+#     max_nam = final_list[0]
+#     min_nam = final_list[0]
+#     for j in final_list:
+#         if j > max_nam:
+#             max_nam = j
+#         elif j < min_nam:
+#             min_nam = j
+    
+#     return f"{max_nam} {min_nam}"
+
+
+# def find_short(s):
+#     list = s.split()
+#     index = len(list[0])
+#     for i in list:
+#         if len(i) < index:
+#             index = len(i)
+#     return index
+
+
+# def friend(x):
+#     list=[]
+#     for i in x:
+#         if len(i) == 4:
+#             list.append(i)
+#     return list
+            
+        
