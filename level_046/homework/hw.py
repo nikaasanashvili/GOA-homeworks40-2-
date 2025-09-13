8615c8b311770ef417825c96a7f9c77b3123ca80
nikaasanashvili <nikan6215@gmail.com>
2025-09-13 20:33:00 +0400

    goa

---
+++ level_046/homework/hw.py
@@ -0,0 +1,41 @@
+# codewars
+
+# def disemvowel(string_):
+#     final_string=''
+#     for i in string_:
+#         if i.lower() not in 'aeiou':
+#             final_string += i
+#     return final_string
+
+
+# def solution(text, ending):
+#     final_ending = ending[::-1]
+#     if final_ending == text[-1:-(len(ending) + 1):-1]:
+#         return True
+#     else:
+#         return False
+
+
+
+
+# def capitals(word):
+#     index=[]
+#     for i in range(len(word)):
+#         if word[i].capitalize() == word[i]:
+#             index.append(i)
+#     return index
+
+
+# def dont_give_me_five(start,end):
+#     list=[]
+#     for i in range(start, end):
+#         if '5'not in str(i) :
+#             list.append(i)    
+#     return len(list)
+
+
+
+
+
+
+
