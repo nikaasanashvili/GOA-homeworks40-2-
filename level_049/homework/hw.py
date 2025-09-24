cad9daf40736d945d4b491c55e15cfdb79d00a28
nikaasanashvili <nikan6215@gmail.com>
2025-09-24 19:01:49 +0400

    goa

---
+++ level_049/homework/hw.py
@@ -0,0 +1,77 @@
+# codewars
+
+# def small_enough(array, limit):
+#     result = True   
+#     for num in array:
+#         if num > limit:
+#             result = False           
+#     return result
+
+
+
+
+# def sum_digits(number):
+#     sum=0
+#     for i in str(number):
+#         if i in '123456789':
+#             sum += int(i)
+#     return sum
+
+
+# def unique_sum(lst):
+#     sum=0
+#     if lst == []:
+#         return None
+#     else:
+#         for i in set(lst):
+#             sum += i
+#     return sum
+
+
+
+# def cap_me(arr):
+#     list=[]
+#     for i in arr:
+#         list.append(i.capitalize())
+#     return list
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
