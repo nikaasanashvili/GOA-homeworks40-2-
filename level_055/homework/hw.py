980c1f7cf8ab08dc568415d12ec4425c49252d73
nikaasanashvili <nikan6215@gmail.com>
2025-10-15 16:56:01 +0400

    goa

---
+++ level_055/homework/hw.py
@@ -0,0 +1,20 @@
+
+# def gimme(input_array):
+#     max_nam=input_array[0]
+#     min_nam=input_array[0]
+#     middle_nam=input_array[0]
+#     for i in input_array:
+#         if i > max_nam:
+#             max_nam = i
+#         elif i < min_nam:
+#             min_nam = i
+#         elif i > min_nam and i < max_nam:
+#             middle_nam = i
+            
+#     return input_array.index(middle_nam)
+
+
+# def gimme(input_array):
+#     middle_nam = sorted(input_array)
+#     ind = middle_nam[1]
+#     return middle_nam.index(ind)
