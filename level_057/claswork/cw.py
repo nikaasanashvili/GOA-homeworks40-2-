b541ba0421fe651812c721809207b83d4823459b
nikaasanashvili <nikan6215@gmail.com>
2025-10-22 22:40:21 +0400

    goa

---
+++ level_057/claswork/cw.py
@@ -0,0 +1,15 @@
+# codewars
+
+# def check_exam(arr1,arr2):
+#     sum=0
+#     for i in range(len(arr1)):
+#         if arr1[i] == arr2[i]:
+#             sum += 4
+#         elif arr2[i] == '':        
+#             sum += 0
+#         elif arr1[i] != arr2[i]:
+#             sum += -1
+    
+#     if sum < 0:
+#         sum = 0
+#     return sum
--- level_057/homework/hw.py
+++ level_057/homework/hw.py
@@ -4,3 +4,52 @@
 #     return sorted(set(arr1 + arr2))
 
 
+# def nearest_sq(n):
+#     namber=1
+#     while namber * namber < n:
+#         namber += 1
+#     max_namber = namber * namber
+#     min_kvadrati = namber-1
+#     min_namber = min_kvadrati * min_kvadrati
+#     if min_namber == n:
+#         return n
+#     elif n - min_namber < max_namber - n:
+#         return min_namber
+#     else:
+#         return max_namber
+
+# def pythagorean_triple(integers):
+#     nums = sorted(integers)
+#     return nums[0]**2 + nums[1]**2 == nums[2]**2
+
+
+# def first_non_consecutive(arr):
+#     for i in range (len(arr)):
+#         if arr[i] != arr[-1]:
+#             if arr[i] + 1 != arr[ i + 1 ]:
+#                 return arr[i + 1]
+
+
+# def to_csv_text(array):
+#     str_list=''
+#     for i in array:
+#         for j in i:
+#             str_list += str(j) + ','
+#         str_list = str_list[:-1]
+#         str_list += '\n'  
+#     return str_list[:-1]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
