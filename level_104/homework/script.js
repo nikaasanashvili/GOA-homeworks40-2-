423a6c53bdfdb65536cd0cd7f0af56ba831d09c4
nikaasanashvili <nikan6215@gmail.com>
2026-04-25 18:23:22 +0400

    goa

---
+++ level_104/homework/script.js
@@ -0,0 +1,19 @@
+// 1)კონსოლში გამოიტანეთ თქვენის სახელი , ასაკი , სიმაღლე(ათწილადი) და ერთი boolean ტიპის მნიშნელობა
+console.log('nika asanashvili')
+console.log(18)
+console.log(1,65)
+console.log(true)
+
+// 2)შექმენი სამი ცვლადი ერთი LET ის გამოყენებით და შიგნით შეინახე შენის სახელი , მეორე შექმენი CONST ით და  
+// შიგნით შეინახე სიტყვა "LASHA", შემდეგიშექმენი VAR ით და შეინახე რაიმე სხვა სახელი
+// შენი დავალებაა რომ მოახდინო სამივე ცვლადში შენახულო მნშვნელობების კონკატიანცია და გამოიტანო კონსოლში
+
+let name1 = 'nika'
+let name2 = 'saba'
+let name3 = 'daviti'
+const name_conset = 'lasha'
+var name_var = 'dachi'
+
+let indicate = name1 + " " + name2 + " " + name3 + " " + name_conset + " " + name_var;
+console.log(indicate)
+
