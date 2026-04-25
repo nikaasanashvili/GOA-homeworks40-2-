73024ed808779f6ddebbc7510862be4e74982c63
nikaasanashvili <nikan6215@gmail.com>
2026-04-25 19:15:46 +0400

    goa

--- level_104/homework/script.js
+++ level_104/homework/script.js
@@ -1,5 +1,5 @@
-// 1) console.log() არის ფუნქცია, რომელიც გამოიყენება ინფორმაციის გამოსატანად კონსოლში (browser console-ში ან terminal-ში)
-// მას ვიყენებთ კოდის დასატესტად, შეცდომების მოსაძებნად და მნიშვნელობების სანახავად
+// 1) console.log() არის ფუნქცია, რომელიც გამოაქვს კოდის შედეგი
+
 
 // 2) შენი სახელი
 console.log("Nika");
@@ -10,8 +10,7 @@ console.log(17);
 // 4) შენი სიმაღლე (მაგალითად)
 console.log(175);
 
-// 5) ცვლადი არის ადგილი მეხსიერებაში, სადაც ვინახავთ მნიშვნელობას
-// JS-ში ცვლადებს ვქმნით 3 keyword-ით:
+// 5) ცვლადში ვინახავთ სხვადასხვა მონაცემთა ტიპებს რომელიც შეგვიძლია გამოვიყენოთ მოგვიანებით
 var a = 10;
 let b = 20;
 const c = 30;
@@ -25,7 +24,7 @@ const z = 15;
 
 // 8) სხვადასხვა ტიპები
 let str = "hello";     
-let num = 100;         
+let num = 5;         
 let bool = true;      
 
 console.log(str);
@@ -45,21 +44,10 @@ console.log(num1 / num2);
 console.log(num1 % num2);
 
 // 11) მათემატიკური ოპერატორები მაგალითები
-console.log(5 + 2);
-console.log(10 + 3);
-console.log(7 + 8);
-
-console.log(10 - 5);
-console.log(20 - 4);
-console.log(9 - 1);
-
-console.log(2 * 3);
-console.log(4 * 5);
-console.log(6 * 7);
-
+console.log(15 + 21);
+console.log(1 - 25);
+console.log(7 * 3);
 console.log(10 / 2);
-console.log(9 / 3);
-console.log(8 / 4);
 
 // 12) სახელის გაერთიანება
 let firstName = "Nika";
@@ -69,4 +57,4 @@ console.log(firstName + " " + lastName);
 
 // 13)
 let name1 = "Nika";
-console.log("მე მქვია " + name1);
+console.log("me mqvia" +' '+ name1);
