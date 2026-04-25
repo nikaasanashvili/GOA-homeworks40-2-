3dc15929a792666185b8767369a05170028d812e
nikaasanashvili <nikan6215@gmail.com>
2026-04-25 19:07:33 +0400

    goa

--- level_104/homework/script.js
+++ level_104/homework/script.js
@@ -1,19 +1,72 @@
-// 1)კონსოლში გამოიტანეთ თქვენის სახელი , ასაკი , სიმაღლე(ათწილადი) და ერთი boolean ტიპის მნიშნელობა
-console.log('nika asanashvili')
-console.log(18)
-console.log(1,65)
-console.log(true)
+// 1) console.log() არის ფუნქცია, რომელიც გამოიყენება ინფორმაციის გამოსატანად კონსოლში (browser console-ში ან terminal-ში)
+// მას ვიყენებთ კოდის დასატესტად, შეცდომების მოსაძებნად და მნიშვნელობების სანახავად
 
-// 2)შექმენი სამი ცვლადი ერთი LET ის გამოყენებით და შიგნით შეინახე შენის სახელი , მეორე შექმენი CONST ით და  
-// შიგნით შეინახე სიტყვა "LASHA", შემდეგიშექმენი VAR ით და შეინახე რაიმე სხვა სახელი
-// შენი დავალებაა რომ მოახდინო სამივე ცვლადში შენახულო მნშვნელობების კონკატიანცია და გამოიტანო კონსოლში
+// 2) შენი სახელი
+console.log("Nika");
 
-let name1 = 'nika'
-let name2 = 'saba'
-let name3 = 'daviti'
-const name_conset = 'lasha'
-var name_var = 'dachi'
+// 3) შენი ასაკი
+console.log(17);
 
-let indicate = name1 + " " + name2 + " " + name3 + " " + name_conset + " " + name_var;
-console.log(indicate)
+// 4) შენი სიმაღლე (მაგალითად)
+console.log(175);
 
+// 5) ცვლადი არის ადგილი მეხსიერებაში, სადაც ვინახავთ მნიშვნელობას
+// JS-ში ცვლადებს ვქმნით 3 keyword-ით:
+var a = 10;
+let b = 20;
+const c = 30;
+
+// 6) განსხვავება:
+
+// 7) სამივე keyword-ით ცვლადები
+var x = 5;
+let y = 10;
+const z = 15;
+
+// 8) სხვადასხვა ტიპები
+let str = "hello";     
+let num = 100;         
+let bool = true;      
+
+console.log(str);
+console.log(num);
+console.log(bool);
+
+// 9) ორი რიცხვი
+let num1 = 10;
+let num2 = 3;
+
+console.log(num1 + num2); 
+console.log(num1 - num2); 
+console.log(num1 * num2); 
+console.log(num1 / num2); 
+
+// 10) ნაშთი
+console.log(num1 % num2);
+
+// 11) მათემატიკური ოპერატორები მაგალითები
+console.log(5 + 2);
+console.log(10 + 3);
+console.log(7 + 8);
+
+console.log(10 - 5);
+console.log(20 - 4);
+console.log(9 - 1);
+
+console.log(2 * 3);
+console.log(4 * 5);
+console.log(6 * 7);
+
+console.log(10 / 2);
+console.log(9 / 3);
+console.log(8 / 4);
+
+// 12) სახელის გაერთიანება
+let firstName = "Nika";
+let lastName = "Asanashvili";
+
+console.log(firstName + " " + lastName);
+
+// 13)
+let name1 = "Nika";
+console.log("მე მქვია " + name1);
