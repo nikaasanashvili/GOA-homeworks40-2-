9312297e05296a592bc5bf0e650967f45ff6824f
nikaasanashvili <nikan6215@gmail.com>
2026-05-02 17:58:10 +0400

    goa

---
+++ level_105/claswork/script.js
@@ -0,0 +1,11 @@
+// 1)შექმენი სამი ცვლადი,
+// ერთი var ერთი const და ერთი let keyword ის გამოყენებით,ცვლადები შექმენით camelCase ით(ორი ან მეტი სიტყვა იყოს სამივე ცვლადში,
+// სამივე ცვლადში შეინახეთ რაიმე სტრინგი
+// შენი დავალებაა რომ გამოიტანო სამივე ცვლადში შენახული სტრინგების სიგრძე
+var saxeliGvari = 'nika asasnashvili'
+const sayvareliXili = 'sazamtro'
+let sayvareliSawmeli = 'mwvadi'
+
+console.log(saxeliGvari.length)
+console.log(sayvareliSawmeli.length)
+console.log(sayvareliXili.length)
---
+++ level_105/homework/script.js
@@ -0,0 +1,41 @@
+// 1)კომენტარის სახით ახსენით თუ რა არის .length 
+// Tvlis cvladSi simboloebis raodenobas-
+
+
+// 2)
+let str = "hello";
+// დაბეჭდე რამდენი სიმბოლოა
+console.log(str.length)
+    
+// 3)შექმენი ცვლადი სამივე keyword ის გამოყენებით,შენი დავალებაა დაბეჭდო სამივე ცვლადში
+//  შენახული სიმბოლოების ჯამი(გამოიყენე .length და მათემატიკური ოპერატორი)
+var saxeliGvari = 'nika asasnashvili'
+const sayvareliXili = 'sazamtro'
+let sayvareliSawmeli = 'mwvadi'
+
+let jami = saxeliGvari.length + sayvareliXili.length + sayvareliSawmeli.length
+console.log(jami)
+
+
+// 4)შექმენი ორი ერთში შეინახე სახელი მეორეში გვარი ,შემდეგ მოახდინე კონკატინაცია,
+// გამოიყენე string interoplation და ბოლოს დაბეჭდე ამ მთლიანი სტრინგის სიგრძე
+
+
+// 5)შექმენი ცვლადი let ის გამოყენებითდა შეინახე შენი სახელი,შემდეგ განაახლე ცვლადის მნიშნველბა და მიანიჭე მას "nika" დაბეჭდე საბოლოო შედეგი
+
+let saxeli = 'saba'
+let saxeli = 'nika'
+console.log(saxeli)
+
+// 6)შექმენი ცვლადი var ის გამოყენებითდა შეინახე შენი სახელი,შემდეგ განაახლე ცვლადის მნიშნველბა და მიანიჭე მას "giorgi" დაბეჭდე საბოლოო შედეგი
+
+var chemiSaxeli = 'nika'
+var chemiSaxeli = 'giorgi'
+console.log(chemiSaxeli)
+
+// 7)შექმენიც ცვლადი const ის გამოყენებით,შემდეგ შეცვალე მისი მნიშვნელობა ,რატომ არ შეცვალა მნიშვნელობა?
+
+const string = 'txt'
+const string = 'nika'
+console.log(string)
+
