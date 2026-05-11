2d92417515984101c63a597b9e01f865b11d26f9
nikaasanashvili <nikan6215@gmail.com>
2026-05-11 16:20:55 +0400

    goa

---
+++ level_109/homework/hw.py
@@ -0,0 +1,19 @@
+# codewars
+
+# def reverse_number(n):
+#     lst=[]
+#     revers=''
+#     for i in str(n):
+#         if i != '-':
+#             lst.append(i)
+#         else:
+#             revers += i
+#     num = ''.join(lst[::-1])
+#     return int(revers + num)
+
+
+# def digitize(n):
+#     lst=[]
+#     for i in str(n):
+#         lst.append(int(i))
+#     return lst
